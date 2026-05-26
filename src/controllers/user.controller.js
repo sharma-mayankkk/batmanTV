aa4af8f5be4e623f3c8ccffab54f6c5ddb928ce4
sharma-mayankkk <br10216030515@gmail.com>
2026-05-26 13:24:16 +0530

    Implement user registration logic with file uploads and validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,72 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
+import { apiError } from '../utils/apiError.js'
+import { User } from '../models/user.model.js'
+import { uploadOnCloudinary } from '../utils/cloudinary.js'
+import { apiResponse } from '../utils/apiResponse.js'
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message:"ok"
+    //get user details from frontend
+    //validation - not empty
+    //check if user already exist : username / email
+    //check for images 
+    //check for avatars
+    //upload them to cloudinary, avatar
+    //create user object - create entry in DB
+    //remove password and refresh token field from response 
+    //check for user creation 
+    //return response 
+
+    const { fullName, email, username, password } = req.body
+    console.log("Email", email)
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new apiError(400, "All Fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
     })
 
+    if (existedUser) {
+        throw new apiError(409, "User with email or username already exist")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if (!avatarLocalPath) {
+        throw new apiError(400, "Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new apiError(400, "Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatat: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new apiError(500, "Something went wrong while registering user")
+    }
+
+    return res.status(201).json(
+        new apiResponse(200, createdUser, "User registered Successfully")
+    )
 })
 
-export {registerUser}  
+export { registerUser }  
