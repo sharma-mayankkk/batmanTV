2fa15571c5ec1f6ec999d4ce52622b3ae3fe724e
sharma-mayankkk <br10216030515@gmail.com>
2026-06-02 14:48:13 +0530

    Fix import statements in auth middleware and ensure next() is called in user model password hashing

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,12 @@
-import { User } from "../models/user.model";
-import { apiError } from "../utils/apiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.model.js";
+import { apiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 
 
 export const verifyJWT = asyncHandler(async(req,_,next)=>{
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer"," ")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
     
         if(!token){
             throw new apiError(401,"Unauthorized request")
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -60,6 +60,7 @@ userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return;
 
     this.password = await bcrypt.hash(this.password, 10)
+    next()
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
