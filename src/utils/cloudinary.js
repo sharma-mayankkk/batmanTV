d0bae93de7e0a5aee4a08d29c6b98baee1449fc7
sharma-mayankkk <br10216030515@gmail.com>
2026-06-12 18:33:57 +0530

    Refactor deleteFromCloudinary function to accept resource type as a parameter

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -29,11 +29,19 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-const deleteFromCloudinary = async (publicId) => {
+const deleteFromCloudinary = async (
+    publicId,
+    resourceType = "image"
+) => {
     try {
-        if (!publicId) return null;
+        if (!publicId) return null
 
-        const response = await cloudinary.uploader.destroy(publicId);
+        const response = await cloudinary.uploader.destroy(
+            publicId,
+            {
+                resource_type: resourceType
+            }
+        )
 
         return response
     } catch (error) {
