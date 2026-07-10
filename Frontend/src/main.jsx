ae4da417155df47ec476c451888eee0741e42972
sharma-mayankkk <br10216030515@gmail.com>
2026-07-10 16:36:29 +0530

    feat: wrap App component with Redux Provider for state management

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
+import { store } from "./store";
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
