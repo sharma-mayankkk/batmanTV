575cdcb3f5f5a4890262902054068069fab55f20
sharma-mayankkk <br10216030515@gmail.com>
2026-07-11 17:50:22 +0530

    feat: implement user restoration on app load with Redux integration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import AppRoutes from "./routes/AppRoutes";
+import { login } from "./store/slices/authSlice";
+import { getCurrentUser } from "./api/auth";
 
 function App() {
-    return (
-        <>
-            <AppRoutes />
-        </>
-    );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const restoreUser = async () => {
+      try {
+        const user = await getCurrentUser();
+        dispatch(login(user));
+      } catch (error) {
+        // User is not logged in or token expired.
+        // Nothing to do.
+      }
+    };
+
+    restoreUser();
+  }, [dispatch]);
+
+  return <AppRoutes />;
 }
 
 export default App;
