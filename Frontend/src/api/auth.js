759158e972b3c816ef2c6e3e261dab8defb197bf
sharma-mayankkk <br10216030515@gmail.com>
2026-07-11 17:49:38 +0530

    feat: add logoutUser function to authentication API

--- Frontend/src/api/auth.js
+++ Frontend/src/api/auth.js
@@ -4,3 +4,13 @@ export const loginUser = async (credentials) => {
   const { data } = await axios.post("/users/login", credentials);
   return data.data;
 };
+
+export const getCurrentUser = async () => {
+  const { data } = await axios.get("/users/current-user");
+  return data.data;
+};
+
+export const logoutUser = async () => {
+  const { data } = await axios.post("/users/logout");
+  return data;
+};
