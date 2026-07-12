10438be5caaa51d1df4f546dbc3dbaa8223f2c2f
sharma-mayankkk <br10216030515@gmail.com>
2026-07-12 13:44:02 +0530

    feat: add Plyr theme styles and import Plyr CSS for enhanced media playback

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import "plyr-react/plyr.css";
 
 import "./index.css";
 import App from "./App";
