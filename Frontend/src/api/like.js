1043501f570387eac510cab83ec4c001bf3b633e
sharma-mayankkk <br10216030515@gmail.com>
2026-08-09 23:48:00 +0530

    fix: correct formatting and ensure consistent code structure in like.js

--- Frontend/src/api/like.js
+++ Frontend/src/api/like.js
@@ -1,6 +1,14 @@
 import api from "./axios";
 
 export const toggleVideoLike = async (videoId) => {
-  const response = await api.post(`/likes/toggle/video/${videoId}`);
-  return response.data.data;
+    const response = await api.post(`/likes/toggle/video/${videoId}`);
+    return response.data.data;
+};
+
+export const getLikedVideos = async (page = 1, limit = 10) => {
+    const response = await api.get(
+        `/likes/videos?page=${page}&limit=${limit}`
+    );
+
+    return response.data.data;
 };
